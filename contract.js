2cb0df8cebb55a8774d8b8d07e1d76d35e73b210
Michael Yang <themichaelyang@gmail.com>
2017-04-17 02:44:26 -0400

    added contract checking on original function scope

--- contract.js
+++ contract.js
@@ -23,15 +23,61 @@ function defineFunction(definition) {
   }
 }
 
+function checkParameters(fn) {
+  return (...parameters) => {
+    if (fn.length !== parameters.length) {
+      throw new Error("Incorrect number of parameters.");
+    }
+    else {
+      let results = fn(...parameters);
+      return results;
+    }
+  }
+}
+
+function define(definition) {
+  if (definition.input() === false) {
+    throw new Error('Input contract violation.');
+  }
+
+  if (definition.output() === false) {
+    throw new Error('Output contract violation.');
+  }
+
+  return checkParameters(definition.fn);
+}
+
 let add1 = defineFunction({
-  func: (a) => {
-    return a + 1;
-  },
   // contracts expect booleans -- but should they instead expect errors?
   // therefore contract breaks would be more contextual
   // also consider improving this syntax
-  input: (a) => { return Number.isSafeInteger(a); },
-  output: (out) => { return Number.isSafeInteger(out); }
+  input: (input) => { return isNumber(input); },
+  output: (output) => { return isNumber(output); },
+  func: (value) => {
+    return value + 1;
+  }
 });
 
-exports.add = add1;
+let multiply = function(x, y) {
+  let func = (x, y) => { return x * y };
+
+  return define({
+    input: () => { return isNumber(x) && isNumber(y) },
+    output: () => { return isNumber(func(x, y)) },
+    fn: func
+  })(...arguments);
+}
+
+exports.multiply = multiply;
+
+// check if is a primitive number
+// all number values including the infinities
+// but not NaN
+function isNumber(value) {
+  if (Number.isNaN(value)) {
+    return false;
+  }
+  else {
+    return (typeof value) === 'number';
+  }
+}

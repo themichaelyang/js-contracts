f5ffe03bb60bc4733a6ed12508b436f983e7b384
Michael Yang <themichaelyang@gmail.com>
2017-04-17 04:18:06 -0400

    removed experiment with local contracts, updated higher order function example

--- contract.js
+++ contract.js
@@ -1,16 +1,29 @@
-'use strict';
+// doesn't work with rest parameters
+function guardParameters(fn) {
+  return (...parameters) => {
+    if (fn.length !== parameters.length) {
+      throw new Error("Incorrect number of parameters.");
+    }
+    else {
+      let results = fn(...parameters);
+      return results;
+    }
+  }
+}
 
+// consider changing to take in fn, contract
+// so contracts can be duplicated
 function defineFunction(definition) {
   return (...params) => {
-    // will prevent rest variables -- use lists instead?
-    if (definition.func.length !== params.length) {
+    // also doesnt work with rest params
+    if (definition.fn.length !== params.length) {
       throw new Error("Incorrect number of parameters.");
     }
 
-    if (definition.input(...params)) {
-      let result = definition.func(...params);
+    if (guardParameters(definition.input)(...params)) {
+      let result = definition.fn(...params);
 
-      if (definition.output(result)) { // but doesn't work for mutable data
+      if (guardParameters(definition.output)(result)) { // but doesn't work for mutable data
         return result;
       }
       else {
@@ -25,89 +38,24 @@ function defineFunction(definition) {
   }
 }
 
-function checkParameters(fn) {
-  return (...parameters) => {
-    if (fn.length !== parameters.length) {
-      throw new Error("Incorrect number of parameters.");
-    }
-    else {
-      let results = fn(...parameters);
-      return results;
-    }
-  }
-}
-
-let contractChecking = true;
-
-// for reusable contracts, maybe the signature should be
-// define(contracts, fn)
-function define(definition) {
-  if (definition.input() === false && contractChecking) {
-    throw new Error('Input contract violation.');
-  }
-
-  if (definition.output() === false && contractChecking) {
-    throw new Error('Output contract violation.');
-  }
-
-  return checkParameters(definition.fn);
-}
-
-let add1 = defineFunction({
-  // contracts expect booleans -- but should they instead expect errors?
-  // therefore contract breaks would be more contextual
-  // also consider improving this syntax
-  input: (input) => { return isNumber(input); },
-  output: (output) => { return isNumber(output); },
-  func: (value) => {
-    return value + 1;
+// maybe should use a chainable definition instead of nested
+let createMultiplyBy = defineFunction({
+  fn: (value) => {
+    return defineFunction({
+      fn: (x) => {
+        return x * value;
+      },
+      input: (x) => { return isNumber(x); },
+      output: (out) => { return isNumber(out); }
+    });
+  },
+  input: (input) => {
+    return isNumber(input);
+  },
+  output: (result) => {
+    return isFunction(result);
   }
 });
-
-// this syntax is still kind of messy
-let multiply = function(x, y) {
-  let func = (x, y) => { return x * y };
-
-  return define({
-    input: () => { return isNumber(x) && isNumber(y) },
-    output: () => { return isNumber(func(x, y)) },
-    fn: func
-  })(...arguments);
-}
-
-// higher order function test
-// too many nested functions
-let createMultiplyBy = function(value) {
-  let unguardedCreateMultiplyBy = (val) => {
-
-    let multiplyBy = function(x) {
-      let unguardedMultiplyBy = (x) => {
-        return x * val;
-      }
-
-      return define({
-        input: () => { return isNumber(x); },
-        output: () => {
-          return isNumber(unguardedMultiplyBy(x));
-        },
-        fn: unguardedMultiplyBy
-      })(...arguments);
-    };
-
-    return multiplyBy;
-
-  };
-
-  return define({
-    input: () => {
-      return isNumber(value);
-    },
-    output: () => {
-      return isFunction(unguardedCreateMultiplyBy(value));
-    },
-    fn: unguardedCreateMultiplyBy
-  })(...arguments);
-};
 
 exports.createMultiplyBy = createMultiplyBy;
 
